577acea2ca605997dc38df1b6695e572627428b2
Allison Johanson <allymjohanson@gmail.com>
2026-01-23 21:21:20 -0700

    yay it passed

--- shared/src/main/java/chess/moveCalculator/ChessMovesCalculator.java
+++ shared/src/main/java/chess/moveCalculator/ChessMovesCalculator.java
@@ -14,12 +14,49 @@ public class ChessMovesCalculator {
         this.type = type;
     }
 
+    private Collection<ChessMove> moveDirection(ChessBoard board,
+                                                ChessPosition myPosition,
+                                                int rDirection,
+                                                int cDirection){
+        Collection<ChessMove> moves = new java.util.ArrayList<>(List.of());
+        int row = myPosition.getRow()+rDirection;
+        int col = myPosition.getColumn()+cDirection;
+        while (row > 0 && row <= 8 && col > 0 && col <= 8){
+            ChessPosition pos = new ChessPosition(row,col);
+            ChessPiece piece = board.getPiece(pos);
+            if (piece == null) {
+                moves.add(new ChessMove(myPosition, pos, null));
+            } else if (piece.getTeamColor() != pieceColor) {
+                moves.add(new ChessMove(myPosition, pos, null));
+                break;
+            } else{
+                break;
+            }
+            if(type == ChessPiece.PieceType.KING){
+                break;
+            }
+            row += rDirection;
+            col += cDirection;
+        }
+        return moves;
+    }
+
     public Collection<ChessMove> getDiagonal(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = new java.util.ArrayList<>(List.of());
+        moves.addAll(moveDirection(board, myPosition, 1, 1));
+        moves.addAll(moveDirection(board, myPosition, -1, -1));
+        moves.addAll(moveDirection(board, myPosition, 1, -1));
+        moves.addAll(moveDirection(board, myPosition, -1, 1));
+        return moves;
     }
 
     public Collection<ChessMove> getSideways(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> moves = new java.util.ArrayList<>(List.of());
+        moves.addAll(moveDirection(board, myPosition, 1, 0));
+        moves.addAll(moveDirection(board, myPosition, -1, 0));
+        moves.addAll(moveDirection(board, myPosition, 0, -1));
+        moves.addAll(moveDirection(board, myPosition, 0, 1));
+        return moves;
     }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
