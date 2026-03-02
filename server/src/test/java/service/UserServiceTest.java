a7d46428262ed29ecf5861c62c420f1cf914fced
Allison Johanson <allymjohanson@gmail.com>
2026-03-02 14:38:40 -0700

    added tests for structure

---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+}
