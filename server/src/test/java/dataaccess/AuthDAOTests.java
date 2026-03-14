a87b7867af24c559b41179325648055ef75b446a
Allison Johanson <allymjohanson@gmail.com>
2026-03-13 18:32:11 -0600

    auth test corrections

--- server/src/test/java/dataaccess/AuthDAOTests.java
+++ server/src/test/java/dataaccess/AuthDAOTests.java
@@ -5,6 +5,11 @@ import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AuthDAOTests {
     MySqlAuthDAO dao = new MySqlAuthDAO();
 //    UserData user1 = new UserData("name", "password", "email");
@@ -21,6 +26,7 @@ public class AuthDAOTests {
     @Test
     public void createAuthNegative() throws DataAccessException {
         dao.clear();
+        Assertions.assertThrows(RuntimeException.class, () -> dao.createAuth(null));
     }
 
     @Test
@@ -40,7 +46,6 @@ public class AuthDAOTests {
     @Test
     public void deleteSessionPositive() throws DataAccessException {
         dao.clear();
-        dao.clear();
         AuthData auth = dao.createAuth("user1");
         Assertions.assertDoesNotThrow(() -> dao.deleteSession(auth.getAuthToken()));
     }
@@ -48,8 +53,19 @@ public class AuthDAOTests {
     @Test
     public void deleteSessionNegative() throws DataAccessException {
         dao.clear();
-        AuthData auth = dao.createAuth("user1");
+        dao.createAuth("user1");
+        dao.createAuth("user2");
+        dao.createAuth("user3");
         Assertions.assertDoesNotThrow(() -> dao.deleteSession(null));
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement preparedStatement = conn.prepareStatement("SELECT COUNT(*) FROM sessions")) {
+                ResultSet rs = preparedStatement.executeQuery();
+                rs.next();
+                Assertions.assertEquals(3, rs.getInt(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
