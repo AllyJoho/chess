078b2c6e2e329d78f6e1e231f43351191296b206
Allison Johanson <allymjohanson@gmail.com>
2026-03-26 23:28:01 -0600

    will this make it compile

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,18 +1,17 @@
 package client;
 
 import server.ServerFacade;
-import server.Server;
 
 public class ClientMain {
     public static void main(String[] args) {
         String serverUrl = "http://localhost:8040";
         if(args.length == 1){
             serverUrl = args[0];
-        }else{
-            Server server = new Server();
-            var port = server.run(0);
-            System.out.println("Started test HTTP server on " + port);
-            serverUrl = "http://localhost:" + port;
+//        }else{
+//            Server server = new Server();
+//            var port = server.run(0);
+//            System.out.println("Started test HTTP server on " + port);
+//            serverUrl = "http://localhost:" + port;
         }
         new Repl(serverUrl).run();
     }
